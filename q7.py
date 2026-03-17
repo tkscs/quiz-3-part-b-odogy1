115cf644a2b0620ea79a9c77d4dda40585bf9cf8
odogy1 <ocharrette2028@kehillahstudent.org>
2026-03-17 10:37:09 -0700

    Quizz

---
+++ q7.py
@@ -0,0 +1,8 @@
+# Question 6
+
+import random
+y = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+x = random.sample(y, k=3)
+
+
+print(x)
---
+++ q8.py
@@ -0,0 +1,20 @@
+# Question 7
+
+
+word = "abba"
+wordlenght = 4
+#User needs to update wordlenght
+wordlenght += 1
+
+def check_palindrome():
+    
+    
+    for character in word():
+        wordlenght = wordlenght - 1
+
+        if character == word.len(wordlenght):
+            return True
+        else:
+            return False
+
+print(check_palindrome())
